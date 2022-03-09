08e89cdaf82f7bf6fd773e15d0f69d58854ab862
Jiwon Kim <lost731@naver.com>
2022-03-09 21:21:26 +0900

    Add: star-wars-app

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# udemy-REACT-QUERY
-Code to support the Udemy course [React Query: Server State Management in React](https://www.udemy.com/course/learn-react-query/?referralCode=1479BB9FA7CA6F31671E)
+# REACT-QUERY-GROUND
+
+- [ ] learn useQuery, useMutation hooks making blogs
+- [ ] learn useInfinityQuery by making star wars app
--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,13 +1,44 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Species } from "./Species";
+import InfiniteScroll from 'react-infinite-scroller';
+import { useInfiniteQuery } from 'react-query';
+import { Species } from './Species';
 
-const initialUrl = "https://swapi.dev/api/species/";
+const initialUrl = 'https://swapi.dev/api/species/';
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
 };
 
 export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const { data, isLoading, isError, error, hasNextPage, fetchNextPage } = useInfiniteQuery(
+    'star-wars-species',
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+    }
+  );
+
+  if (isLoading) return <div className='loading'>Loading...</div>;
+  if (isError) {
+    return (
+      <div>
+        <h3>Something Wrong...</h3>
+        <p>{error.toString()}</p>
+      </div>
+    );
+  }
+
+  return (
+    <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+      {data.pages.map((pageData) =>
+        pageData.results.map((result) => (
+          <Species
+            key={result.name}
+            name={result.name}
+            language={result.language}
+            averageLifespan={result.average_lifespan}
+          />
+        ))
+      )}
+    </InfiniteScroll>
+  );
 }
